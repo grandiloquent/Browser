cfceb3235e906b9dd4642917dd9bc481dec39058
grandiloquent <1785907865@qq.com>
2020-06-12 06:01:51 +0800

    changed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# Browser" 
+# Browser 文件管理器
+
+[x] Web 服务器
+
+## 参考
+
+- https://www.man7.org/linux/man-pages/man3/getaddrinfo.3.html
--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -9,35 +9,16 @@
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-int create_socket(const char *host, const char *port) {
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
 
-    struct addrinfo *bind_address;
-    getaddrinfo(host, port, &hints, &bind_address);
-    int socket_listen;
-    socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype,
-                           bind_address->ai_protocol);
-    if (socket_listen == 0) {
-        LOGE("socket() failed. (%d)\n", errno);
-        goto err;
-    }
-    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
-        LOGE("bind() failed. (%d)\n", errno);
-        goto err;
-    }
-    freeaddrinfo(bind_address);
-    if (listen(socket_listen, 10) < 0) {
-        LOGE("listen() failed. (%d)\n", errno);
-        goto err;
-    }
-    return socket_listen;
-    err:
-    freeaddrinfo(bind_address);
-    return 0;
+int tcp_connect(const char *host, const char *port) {
+    int ret;
+    struct addrinfo hints, *addr_list, *cur;
+    int fd = 0;
+    memset(&hints, 0x00, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+
 }
 
 JNIEXPORT jboolean JNICALL
