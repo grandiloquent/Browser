71de6f36c8dbb1820c5a891d0c92ae98f5b9cd21
grandiloquent <1785907865@qq.com>
2020-06-12 11:10:28 +0800

    changed

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 * [ ] Web 服务器
 
+
+## 引用
+
+- https://github.com/cesanta/mongoose
+
 ## 参考
 
 - https://www.man7.org/linux/man-pages/man3/getaddrinfo.3.html
--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -1,56 +1,50 @@
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <netinet/tcp.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-
 #include "jni.h"
 #include "android/log.h"
+#include "mongoose.h"
 
 #define LOG_TAG "TAG/Native"
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+static struct mg_serve_http_opts s_http_server_opts;
 
-int create_socket(const char *host, const char *port) {
+static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix) {
+    return uri->len > prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
+}
 
-    struct addrinfo hints;
+static int is_equal(const struct mg_str *s1, const struct mg_str *s2) {
+    return s1->len == s2->len && memcmp(s1->p, s2->p, s2->len) == 0;
+}
 
-    memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    struct addrinfo *bind_address;
-    getaddrinfo(host, port, &hints, &bind_address);
-
-    int socket_listen;
-
-    socket_listen = socket(bind_address->ai_family,
-                           bind_address->ai_socktype, bind_address->ai_protocol);
-    if (socket_listen == 0) {
-        LOGE("socket() failed. (%d)\n", errno);
-        goto err;
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
+    static const struct mg_str api_index = MG_MK_STR("/");
+    static const struct mg_str api_get = MG_MK_STR("/api/get/");
+    static const struct mg_str api_update = MG_MK_STR("/api/update");
+    struct http_message *hm = (struct http_message *) ev_data;
+    if (ev == MG_EV_HTTP_REQUEST) {
+        if (is_equal(&hm->uri, &api_index)) {
+            // index(nc, hm);
+        } else if (has_prefix(&hm->uri, &api_get)) {
+            //GetJSON(nc, hm, atoi(hm->uri.p + api_get.len));
+        } else if (is_equal(&hm->uri, &api_update)) {
+            //UpdateJSON(nc, hm);
+        } else {
+            LOGE("ev_handler: %s\n", s_http_server_opts.document_root);
+            mg_serve_http(nc, hm, s_http_server_opts);
+        }
     }
-    if (
-            bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
-        LOGE("bind() failed. (%d)\n", errno);
-        goto err;
+}
 
+void *start_server(const char *address) {
+    struct mg_mgr mgr;
+    struct mg_connection *nc;
+    mg_mgr_init(&mgr, NULL);
+    LOGE("%s", address);
+    nc = mg_bind(&mgr, address, ev_handler);
+    mg_set_protocol_http_websocket(nc);
+    for (;;) {
+        mg_mgr_poll(&mgr, 500);
     }
-    if (listen(socket_listen, 10) < 0) {
-        LOGE("listen() failed. (%d)\n", errno);
-    }
-    err:
-    freeaddrinfo(bind_address);
-    return socket_listen;
+    mg_mgr_free(&mgr);
 }
 
 JNIEXPORT jboolean JNICALL
@@ -59,7 +53,13 @@ Java_euphoria_psycho_browser_app_NativeHelper_startServer(JNIEnv *env, jclass cl
     const char *host = (*env)->GetStringUTFChars(env, host_, 0);
     const char *port = (*env)->GetStringUTFChars(env, port_, 0);
 
-    create_socket(host, port);
+    char url[256];
+
+    sprintf(url, "%s:%s", host, port);
+
+
+    pthread_t t;
+    pthread_create(&t, NULL, (void *(*)(void *)) start_server, url);
 
     (*env)->ReleaseStringUTFChars(env, host_, host);
     (*env)->ReleaseStringUTFChars(env, port_, port);
