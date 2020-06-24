d8833e2c859b87f9b5fea29cbf8e3b083358247e
grandiloquent <1785907865@qq.com>
2020-06-24 14:57:53 +0800

    更新

--- app/src/main/java/euphoria/psycho/browser/download/DownloadService.java
+++ app/src/main/java/euphoria/psycho/browser/download/DownloadService.java
@@ -4,12 +4,29 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import androidx.annotation.Nullable;
 
 public class DownloadService extends Service {
+    Executor mExecutor;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mExecutor = Executors.newCachedThreadPool();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_NOT_STICKY;
     }
 }
