c5b7c1f783b4b752e33ba925ceee9ba63ed9d329
grandiloquent <1785907865@qq.com>
2021-09-13 01:07:56 +0800

    changed

--- app/src/main/java/euphoria/psycho/browser/app/SampleDownloadActivity.java
+++ app/src/main/java/euphoria/psycho/browser/app/SampleDownloadActivity.java
@@ -23,6 +23,7 @@ import android.widget.Toast;
 import at.huber.youtubeExtractor.VideoMeta;
 import at.huber.youtubeExtractor.YouTubeExtractor;
 import at.huber.youtubeExtractor.YtFile;
+import euphoria.psycho.Share;
 import euphoria.psycho.browser.R;
 import euphoria.psycho.share.ContextUtils;
 import euphoria.psycho.share.Log;
@@ -143,13 +144,15 @@ public class SampleDownloadActivity extends Activity {
     }
 
     private void downloadFromUrl(String youtubeDlUrl, String downloadTitle, String fileName) {
-        Uri uri = Uri.parse(youtubeDlUrl);
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-        request.setTitle(downloadTitle);
-        request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        manager.enqueue(request);
+        ContextUtils.setClipboardString(youtubeDlUrl);
+        
+//        Uri uri = Uri.parse(youtubeDlUrl);
+//        DownloadManager.Request request = new DownloadManager.Request(uri);
+//        request.setTitle(downloadTitle);
+//        request.allowScanningByMediaScanner();
+//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+//        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+//        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+//        manager.enqueue(request);
     }
 }
